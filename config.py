ef3e3ecb4c79317f5c5f22b81784f2139190dff2
Muammer Aktas <maktascs@gmail.com>
2019-05-17 07:50:25 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 
-REDDIT = {'client_id' : "u4sqq7AKnCytuQ",
-'client_secret' : "n0K9p_jLhySAvb82axVngH8uuLw"}
+REDDIT = {'client_id' : "",
+'client_secret' : ""}
 
-TWITTER={'consumer_key' : 'oqHWOWStihsMu6TyezbWBwD7j',
-'consumer_secret' : 'jIqcNn1zsSfdrzZwQ08do5e154mg5p6bKBBwUHEL8t8mjZWO0k',
-'access_token' : '1126881220049166336-8DbgfoqrYtPRVD9RbhF1yNDwZ6RVOS',
-'access_token_secret' : 'x28chBJOlF7tk8ZUFi1aCToAD7u7LuXgNZJMqHNS9Wy6f'}
+TWITTER={'consumer_key' : '',
+'consumer_secret' : '',
+'access_token' : '',
+'access_token_secret' : ''}
